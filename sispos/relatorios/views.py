50a693e3246831ebdb1491b6d01605c77d90c1f6
Marcelo <schneider.fei@gmail.com>
2020-03-24 16:59:48 -0300

    Adiciona  o form ao contexto - Fixes #7

--- sispos/relatorios/views.py
+++ sispos/relatorios/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render
+from sispos.relatorios.forms import Relatorios
 
 
 def relatorios_novo(request):
-    return render(request, 'relatorios_novo.html')
+    form = Relatorios()
+    return render(request, 'relatorios_novo.html', {'form': form})
