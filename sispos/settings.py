86f0145beace18e4ce4e2650972f7ac5806cac49
Marcelo <schneider.fei@gmail.com>
2020-03-24 18:56:23 -0300

    Installa o pacote django-widget-tweaks - Fixes #7

--- sispos/settings.py
+++ sispos/settings.py
@@ -69,6 +69,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'django_extensions',
+    'widget_tweaks',
     'bootstrap4',
     'sispos.core',
     'sispos.accounts',
