45ceed0c25335f559c8d9bf27e0753d1add7db23
Marcelo <schneider.fei@gmail.com>
2020-03-24 16:48:25 -0300

    Adiciona opções ao choice field - Fixes #7

--- sispos/relatorios/forms.py
+++ sispos/relatorios/forms.py
@@ -1,10 +1,15 @@
 from django import forms
 
 
+RELATOR_CHOICES = (('Relator 1', 'Relator 1'), ('Relator 2', 'Relator 2'))
+ORIENTADOR_CHOICES = (('Orientador 1', 'Orientador 1'), ('Orientador 2', 'Orientador 2'))
+PROGRAMA_CHOICES = (('Programa 1', 'Programa 1'), ('Programa 2', 'Programa 2'))
+
+
 class Relatorios(forms.Form):
     nome = forms.CharField(label='Nome', max_length=256)
-    relator = forms.ChoiceField(label='Relator')
-    orientador = forms.ChoiceField(label='Orientador')
-    programa = forms.ChoiceField(label='Programa')
+    relator = forms.ChoiceField(label='Relator', choices=RELATOR_CHOICES)
+    orientador = forms.ChoiceField(label='Orientador', choices=ORIENTADOR_CHOICES)
+    programa = forms.ChoiceField(label='Programa', choices=PROGRAMA_CHOICES)
     relatorio = forms.FileField(label='Relatório')
     encaminhamento = forms.FileField(label='Encaminhamento')
