dfc4d8ebb88efccf797fd5c70e04037becac9959
Marcelo <schneider.fei@gmail.com>
2020-03-24 15:40:39 -0300

    Renderiza formulário - Fixes #7

--- sispos/relatorios/tests/test_relatorios_views.py
+++ sispos/relatorios/tests/test_relatorios_views.py
@@ -17,3 +17,12 @@ class RelatoriosViewsTest(TestCase):
     def test_base_template(self):
         """It should render base.html"""
         self.assertTemplateUsed(self.resp, 'base.html')
+
+    def test_html_form(self):
+        """Template should render html form"""
+        html_form_itens = ((1, '<form'), (1, 'type="text"'), (3, '<select'),
+                           (2, 'type="file"'), (1, 'type="submit"'))
+
+        for count, expected in html_form_itens:
+            with self.subTest():
+                self.assertContains(self.resp, expected, count)
