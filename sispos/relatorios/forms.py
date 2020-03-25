8e539dccec812c53b847fb2dfb320210ffd20046
Marcelo <schneider.fei@gmail.com>
2020-03-25 17:34:49 -0300

    Torna o campo relator não obrigatório - Fixes #11

--- sispos/relatorios/forms.py
+++ sispos/relatorios/forms.py
@@ -3,12 +3,12 @@ from django import forms
 
 RELATOR_CHOICES = (('Relator 1', 'Relator 1'), ('Relator 2', 'Relator 2'))
 ORIENTADOR_CHOICES = (('Orientador 1', 'Orientador 1'), ('Orientador 2', 'Orientador 2'))
-PROGRAMA_CHOICES = (('Programa 1', 'Programa 1'), ('Programa 2', 'Programa 2'))
+PROGRAMA_CHOICES = (('Mestrado', 'Mestrado'), ('Doutorado', 'Doutorado'))
 
 
 class Relatorios(forms.Form):
     nome = forms.CharField(label='Nome', max_length=256)
-    relator = forms.ChoiceField(label='Relator', choices=RELATOR_CHOICES)
+    relator = forms.ChoiceField(label='Relator', choices=RELATOR_CHOICES, required=False)
     orientador = forms.ChoiceField(label='Orientador', choices=ORIENTADOR_CHOICES)
     programa = forms.ChoiceField(label='Programa', choices=PROGRAMA_CHOICES)
     relatorio = forms.FileField(label='Relatório')
