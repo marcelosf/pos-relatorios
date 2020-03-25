c7a1151f83d6b715ecbc46affb030786322c6241
Marcelo <schneider.fei@gmail.com>
2020-03-25 17:35:36 -0300

    Adiciona fução para salvar arquivos - Fixes #11

--- sispos/relatorios/views.py
+++ sispos/relatorios/views.py
@@ -1,10 +1,21 @@
 from django.shortcuts import render
 from django.conf import settings
 from django.contrib.auth.decorators import login_required
-from sispos.relatorios.forms import Relatorios
+from sispos.relatorios.forms import Relatorios as RelatoriosForm
+from sispos.relatorios.models import Relatorios
 
 
 @login_required(login_url=settings.LOGIN_URL)
 def relatorios_novo(request):
-    form = Relatorios()
+    form = RelatoriosForm()
+    if not request.method == 'POST':
+        return render(request, 'relatorios_novo.html', {'form': form})
+    form = create_relatorio(request)
     return render(request, 'relatorios_novo.html', {'form': form})
+
+
+def create_relatorio(request):
+    form = RelatoriosForm(request.POST, request.FILES)
+    if form.is_valid():
+        Relatorios.objects.create(**form.cleaned_data)
+    return form
