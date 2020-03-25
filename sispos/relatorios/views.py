ebf899fe0c670d7f5ab1e7a75078a4d78c874ff6
Marcelo <schneider.fei@gmail.com>
2020-03-25 13:41:23 -0300

    Restringe acesso por login - Fixes #12
     - Apenas usuaŕios logados podem acessar o formulário

--- sispos/relatorios/views.py
+++ sispos/relatorios/views.py
@@ -1,7 +1,10 @@
 from django.shortcuts import render
+from django.conf import settings
+from django.contrib.auth.decorators import login_required
 from sispos.relatorios.forms import Relatorios
 
 
+@login_required(login_url=settings.LOGIN_URL)
 def relatorios_novo(request):
     form = Relatorios()
     return render(request, 'relatorios_novo.html', {'form': form})
