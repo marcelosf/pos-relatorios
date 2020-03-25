564c0a558e992cd2f12b0a1b32513dea50f26825
Marcelo <schneider.fei@gmail.com>
2020-03-25 16:03:52 -0300

    Torna parametro relator igual a blank - Fixes #11

--- sispos/relatorios/models.py
+++ sispos/relatorios/models.py
@@ -3,7 +3,7 @@ from django.db import models
 
 class Relatorios(models.Model):
     nome = models.CharField('Nome', max_length=128)
-    relator = models.CharField('Relator', max_length=128)
+    relator = models.CharField('Relator', max_length=128, blank=True)
     orientador = models.CharField('Orientador', max_length=128)
     programa = models.CharField('Programa', max_length=20)
     relatorio = models.CharField('Relatório', max_length=20)
