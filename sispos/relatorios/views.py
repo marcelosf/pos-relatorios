9525de18ffe02fc9d348f1eb2c608a0516ba8a37
Marcelo <schneider.fei@gmail.com>
2020-03-26 17:38:21 -0300

    Envia email - Fixes #14  - Envia email para o aluno e para o coordenador após submissão do relatório.

--- sispos/relatorios/views.py
+++ sispos/relatorios/views.py
@@ -2,6 +2,8 @@ from django.shortcuts import render
 from django.conf import settings
 from django.contrib.auth.decorators import login_required
 from django.contrib import messages
+from django.template.loader import render_to_string
+from django.core import mail
 from sispos.relatorios.forms import Relatorios as RelatoriosForm
 from sispos.relatorios.models import Relatorios
 
@@ -20,4 +22,19 @@ def create_relatorio(request):
     if form.is_valid():
         Relatorios.objects.create(**form.cleaned_data)
         messages.success(request, 'Relatório enviado com sucesso.')
+        send_email(
+            to=[request.user.main_email],
+            template_name='email_aluno.txt',
+            context={'subscription': form.cleaned_data}
+        )
+        send_email(
+            to=['coordenador1@test.com', 'coordenador2@test.com'],
+            template_name='email_comissao.txt',
+            context={'subscription': {}}
+        )
     return form
+
+
+def send_email(to, template_name, context):
+    body = render_to_string(template_name, context)
+    mail.send_mail(settings.EMAIL_SUBJECT, body, settings.EMAIL_FROM, to)
