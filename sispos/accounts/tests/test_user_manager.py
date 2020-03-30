909666d4267ba9675207583262d1bfce35ccaab4
Marcelo <schneider.fei@gmail.com>
2020-03-30 12:52:37 -0300

    Adiciona espaço entre as funções  - Fixes #14

--- sispos/accounts/tests/test_user_manager.py
+++ sispos/accounts/tests/test_user_manager.py
@@ -6,7 +6,6 @@ class TestUserManager(TestCase):
     def setUp(self):
         self.obj = UserManager()
 
-    
     def test_user_manager(self):
         """It must exists"""
         self.assertIsInstance(self.obj, UserManager)
