9f4039b28967809f70b42598f3065583694e5125
Marcelo <schneider.fei@gmail.com>
2020-03-31 15:52:48 -0300

    Verifica as opções do choiceField - Fixes #25
     - Verifica se as opções dos campos estão sendo carregadas do banco de dados

--- sispos/relatorios/tests/test_relatorios_forms.py
+++ sispos/relatorios/tests/test_relatorios_forms.py
@@ -1,14 +1,17 @@
 from django.test import TestCase
-from sispos.relatorios.forms import Relatorios
+from sispos.relatorios.forms import RelatoriosForm
+from sispos.relatorios.tests import mock
 
 
 class RelatoriosFormTest(TestCase):
     def setUp(self):
-        self.form = Relatorios()
+        mock_orientador = mock.MockUser()
+        self.orientador = mock_orientador.make_orientador()
+        self.form = RelatoriosForm()
 
     def test_instance(self):
         """Instance should be Relatorios"""
-        self.assertIsInstance(self.form, Relatorios)
+        self.assertIsInstance(self.form, RelatoriosForm)
 
     def test_has_fields(self):
         """Form should have fields"""
@@ -18,3 +21,8 @@ class RelatoriosFormTest(TestCase):
         for expected in fields:
             with self.subTest():
                 self.assertIn(expected, list(self.form.fields))
+
+    def test_orientador_choices(self):
+        """It should have a list of orientadores"""
+        expected = [(self.orientador.pk, self.orientador.name)]
+        self.assertListEqual(expected, self.form.fields['orientador'].choices)
