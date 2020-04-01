52e1233d152a99859fc5258418d9671788f547d3
Marcelo <schneider.fei@gmail.com>
2020-04-01 18:14:33 -0300

    Formata botão detalhar - issue #32

--- sispos/relatorios/tables.py
+++ sispos/relatorios/tables.py
@@ -1,9 +1,15 @@
 import django_tables2 as tables
+from django.utils.html import format_html
 from sispos.relatorios.models import Relatorios
 
 
 class RelatoriosTable(tables.Table):
+    uuid = tables.Column(verbose_name='Ação')
+
+    def render_uuid(self, value):
+        return format_html('<a class="btn btn-sm btn-outline-primary" href="#">Detalhar</a>')
+
     class Meta:
         model = Relatorios
         template_name = 'django_tables2/bootstrap4.html'
-        fields = ('nome', 'created', 'orientador')
+        fields = ('nome', 'created', 'orientador', 'uuid')
