80c19665c259c150f8a90bde4e2f5e1ada6ddfde
Marcelo <schneider.fei@gmail.com>
2020-04-01 14:52:17 -0300

    Cria rota para a lista de relatórios - issue #32

--- sispos/relatorios/urls.py
+++ sispos/relatorios/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from sispos.relatorios.views import relatorios_novo
+from sispos.relatorios.views import relatorios_novo, relatorios_list
 
 app_name = 'relatorios'
 
 urlpatterns = [
     path('novo', relatorios_novo, name='relatorios_new'),
+    path('', relatorios_list, name='relatorios_list'),
 ]
