8696b687c2e6ca6bd43a4729c87685c554927661
Marcelo <schneider.fei@gmail.com>
2020-04-02 11:30:38 -0300

    Cria formulário de filtro - issue #32
     - Cria formulário de filtro para a lista de relatórios

--- sispos/relatorios/filters.py
+++ sispos/relatorios/filters.py
@@ -1,11 +1,20 @@
 import django_filters
+from django.contrib.auth.models import Group
 from sispos.relatorios.models import Relatorios
 
 
+def get_orientadores():
+    group_data = {'name': 'orientador'}
+    orientadores_group, created = Group.objects.get_or_create(**group_data)
+    orientadores = orientadores_group.user_set.all()
+    return orientadores
+
+
 class RelatoriosFilter(django_filters.FilterSet):
-    nome = django_filters.CharFilter('Nome')
-    orientador = django_filters.CharFilter('Orientador')
-    created = django_filters.DateFilter('Envio')
+    nome = django_filters.CharFilter(label='Nome')
+    orientador = django_filters.ModelChoiceFilter(queryset=get_orientadores(),
+                                                  label='Orientadores')
+    created = django_filters.DateFilter(label='Envio')
 
     class Meta:
         model = Relatorios
