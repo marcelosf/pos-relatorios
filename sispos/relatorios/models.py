5945052467fb2da55685be45a946bd3812ab5f68
Marcelo Schneider <schneider.fei@gmail.com>
2020-04-07 18:51:20 -0300

    Define relator como ForeignKey - issue #33

--- sispos/relatorios/models.py
+++ sispos/relatorios/models.py
@@ -4,9 +4,13 @@ from sispos.accounts.models import User
 from uuid import uuid4
 
 
+def limit_to_relator():
+    return {'groups__name': 'relatores'}
+
+
 class Relatorios(models.Model):
     nome = models.CharField('Nome', max_length=128)
-    relator = models.CharField('Relator', max_length=128, blank=True)
+    relator = models.ForeignKey(to=User, related_name='relator', on_delete=models.SET_NULL, null=True, limit_choices_to=limit_to_relator)
     orientador = models.CharField('Orientador', max_length=128)
     programa = models.CharField('Programa', max_length=20)
     relatorio = models.FileField('Relatório', upload_to='relatorios/%Y/%m/%d/')
