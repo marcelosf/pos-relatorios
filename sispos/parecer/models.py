f6b683fc68c623e52ca995c003d917f1b8d67d34
Marcelo Schneider <schneider.fei@gmail.com>
2020-04-10 16:53:09 -0300

    Cria model Rds1 - issue #53

--- sispos/parecer/models.py
+++ sispos/parecer/models.py
@@ -1,3 +1,15 @@
 from django.db import models
+from sispos.relatorios.models import Relatorios
+from sispos.accounts.models import User
 
-# Create your models here.
+
+class Rds1(models.Model):
+    desempenho = models.CharField('desempenho', max_length=2048)
+    revisao = models.CharField('revisao', max_length=2048)
+    definicao = models.CharField('definicao', max_length=2048)
+    plano = models.CharField('planos', max_length=2048)
+    resultados = models.CharField('resultados', max_length=2048)
+    atividades = models.CharField('atividades', max_length=2048)
+    relatorio = models.ForeignKey(Relatorios, on_delete=models.CASCADE)
+    relator = models.ForeignKey(User, on_delete=models.CASCADE)
+    created_at = models.DateTimeField(auto_now_add=True)
