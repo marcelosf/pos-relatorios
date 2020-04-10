36f04207a472c69cc98b2c00c17b225f7578fb4f
Marcelo Schneider <schneider.fei@gmail.com>
2020-04-10 20:21:30 -0300

    Substitui widgets para TextField - issue #53

--- sispos/parecer/models.py
+++ sispos/parecer/models.py
@@ -4,12 +4,12 @@ from sispos.accounts.models import User
 
 
 class Rds1(models.Model):
-    desempenho = models.CharField('desempenho', max_length=2048)
-    revisao = models.CharField('revisao', max_length=2048)
-    definicao = models.CharField('definicao', max_length=2048)
-    plano = models.CharField('planos', max_length=2048)
-    resultados = models.CharField('resultados', max_length=2048)
-    atividades = models.CharField('atividades', max_length=2048)
+    desempenho = models.TextField('desempenho', max_length=2048)
+    revisao = models.TextField('revisao', max_length=2048)
+    definicao = models.TextField('definicao', max_length=2048)
+    plano = models.TextField('planos', max_length=2048)
+    resultados = models.TextField('resultados', max_length=2048)
+    atividades = models.TextField('atividades', max_length=2048)
     relatorio = models.ForeignKey(Relatorios, on_delete=models.CASCADE)
     relator = models.ForeignKey(User, on_delete=models.CASCADE)
     created_at = models.DateTimeField(auto_now_add=True)
