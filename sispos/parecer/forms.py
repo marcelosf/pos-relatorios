14184cf1284e52917c6a742fe65d64ea6e78e2f7
Marcelo Schneider <schneider.fei@gmail.com>
2020-04-10 14:25:39 -0300

    Adiciona wigdts ao formulário - ossue #53

--- sispos/parecer/forms.py
+++ sispos/parecer/forms.py
@@ -2,10 +2,16 @@ from django import forms
 
 
 class Rds1Form(forms.Form):
-    desempenho = forms.CharField()
-    revisao = forms.CharField()
-    definicao = forms.CharField()
-    plano = forms.CharField()
-    resultados = forms.CharField()
-    atividades = forms.CharField()
-    relatorio = forms.HiddenInput()
+    desempenho = forms.CharField(label='Desempenho nas disciplinas',
+                                 max_length=2048, widget=forms.Textarea)
+    revisao = forms.CharField(label='Revisão Bibliográfica',
+                              max_length=2048, widget=forms.Textarea)
+    definicao = forms.CharField(label='Definição do problema a ser investigado',
+                                max_length=2048, widget=forms.Textarea)
+    plano = forms.CharField(label='Plano de trabalho detalhado',
+                            max_length=2048, widget=forms.Textarea)
+    resultados = forms.CharField(label='Resultados iniciais',
+                                 max_length=2048, widget=forms.Textarea)
+    atividades = forms.CharField(label='Outras atividades',
+                                 max_length=2048, widget=forms.Textarea)
+    relatorio = forms.CharField(label='relatório', widget=forms.HiddenInput)
