c06dedd2e82308edf168da93a8018190a1bee0a4
Marcelo Schneider <schneider.fei@gmail.com>
2020-04-10 21:13:44 -0300

    Refatora testes e testa status code - issue #53
    - Refatora testes para nova url e testa status code para o verbo post

--- sispos/parecer/tests/test_parecer_views.py
+++ sispos/parecer/tests/test_parecer_views.py
@@ -1,10 +1,15 @@
 from django.test import TestCase
 from django.shortcuts import resolve_url as r
+from sispos.parecer.tests import mock
 
 
 class ParecerViewsGetTest(TestCase):
     def setUp(self):
-        self.resp = self.client.get(r('parecer:parecer_new'))
+        mock_relatorio = mock.MockRelatorio()
+        data = mock_relatorio.make_relatorio()
+        relatorio = mock_relatorio.save_relatorio(data)
+        self.resp = self.client.get(r('parecer:parecer_new',
+                                      slug=str(relatorio.uuid)))
 
     def test_status_code(self):
         """Sttatus code should be 200"""
@@ -33,3 +38,16 @@ class ParecerViewsGetTest(TestCase):
     def test_has_csrf(self):
         """It should render the csrf middleware"""
         self.assertContains(self.resp, 'csrfmiddlewaretoken')
+
+
+class ParecerViewPostTest(TestCase):
+    def setUp(self):
+        mock_relatorio = mock.MockRelatorio()
+        data = mock_relatorio.make_relatorio()
+        relatorio = mock_relatorio.save_relatorio(data)
+        self.resp = self.client.post(r('parecer:parecer_new',
+                                       slug=str(relatorio.uuid)))
+
+    def test_status_code(self):
+        """Status code should be 200"""
+        self.assertEqual(200, self.resp.status_code)
