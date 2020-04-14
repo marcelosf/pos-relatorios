3f6e74c440fbda28a80b2f42af67180c8881fdd1
Marcelo Schneider <schneider.fei@gmail.com>
2020-04-14 17:59:35 -0300

    Refatora views - issue #67

--- sispos/parecer/views.py
+++ sispos/parecer/views.py
@@ -8,46 +8,30 @@ from sispos.parecer.models import Rds1, Rds2
 
 def parecer_new(request, slug):
     if request.method == 'POST':
-        create_rds1(request, slug)
+        create_parecer(request, slug, Rds1, Rds1Form)
     context = {'form': Rds1Form(), 'slug': slug,
                'action': r('parecer:parecer_new', slug=slug)}
-    return render(request, 'parecer_ds1_new.html', context)
-
-
-def create_rds1(request, slug):
-    form = Rds1Form(request.POST)
-    if form.is_valid():
-        relatorio = Relatorios.objects.get(uuid=slug)
-        form.cleaned_data['relatorio'] = relatorio
-        form.cleaned_data['relator'] = request.user
-        Rds1.objects.create(**form.cleaned_data)
-        context = {'form': form, 'slug': slug}
-        messages.success(request, 'Parecer enviado com sucesso.')
-        return render(request, 'parecer_ds1_new.html', context)
-    messages.error(request, 'Não foi possível enviar o parecer.')
-    context = {'form': form, 'slug': slug}
-    return render(request, 'parecer_ds1_new.html', context)
+    return render(request, 'parecer_new.html', context)
 
 
 def parecer_rds2_new(request, slug):
     if request.method == 'POST':
-        create_rds2(request, slug)
+        create_parecer(request, slug, Rds2, Rds2Form)
     context = {'slug': slug, 'form': Rds2Form(),
                'action': r('parecer:parecer_rds2_new', slug=slug)}
     return render(request, 'parecer_new.html', context)
 
 
-def create_rds2(request, slug):
-    form = Rds2Form(request.POST)
+def create_parecer(request, slug, model, form_class):
+    form = form_class(request.POST)
+    context = {'form': form, 'slug': slug,
+               'action': r('parecer:parecer_new.html', slug=slug)}
     if form.is_valid():
         relatorio = Relatorios.objects.get(uuid=slug)
         form.cleaned_data['relatorio'] = relatorio
         form.cleaned_data['relator'] = request.user
-        Rds2.objects.create(**form.cleaned_data)
-        context = {'form': form, 'slug': slug,
-                   'action': r('parecer:parecer_rds2_new', slug=slug)}
+        model.objects.create(**form.cleaned_data)
         messages.success(request, 'Parecer enviado com sucesso.')
         return render(request, 'parecer_new.html', context)
-    context = {'form': form, 'slug': slug,
-               'action': r('parecer:parecer_rds2_new', slug=slug)}
+    messages.error(request, 'Não foi possível enviar o parecer.')
     return render(request, 'parecer_new.html', context)
