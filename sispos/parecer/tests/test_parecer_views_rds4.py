42d551cf518abb1759aa1aec2c73063679bdefed
Marcelo Schneider <schneider.fei@gmail.com>
2020-04-15 16:35:32 -0300

    Verifica status code - issue #73

--- sispos/parecer/tests/test_parecer_views_rds4.py
+++ sispos/parecer/tests/test_parecer_views_rds4.py
@@ -23,3 +23,21 @@ class Rds4ViewGetTest(TestCase):
         """Context must have a form"""
         context = self.resp.context
         self.assertIn('form', context)
+
+
+class Rds4ViewPostTest(TestCase):
+    def setUp(self):
+        mock_user = mock.MockUser()
+        relator = mock_user.make_relator()
+        self.client.force_login(relator)
+        mock_relatorio = mock.MockRelatorio()
+        relatorio_data = mock_relatorio.make_relatorio()
+        relatorio = mock_relatorio.save_relatorio(relatorio_data)
+        mock_rds4 = mock.MockParecer()
+        rds4_data = mock_rds4.make_rds4(relator=relator, relatorio=relatorio)
+        self.resp = self.client.post(r('parecer:parecer_rds4_new',
+                                       slug=str(relatorio.uuid)), rds4_data)
+
+    def test_status_code(self):
+        """Status code must be 200"""
+        self.assertEqual(200, self.resp.status_code)
