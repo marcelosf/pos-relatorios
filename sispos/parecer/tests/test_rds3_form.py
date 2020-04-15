c3a689d4dddf093ae95da775a1188df314aa93ec
Marcelo Schneider <schneider.fei@gmail.com>
2020-04-15 11:17:55 -0300

    Verifica os campos do formulário - issue #71

---
+++ sispos/parecer/tests/test_rds3_form.py
@@ -0,0 +1,14 @@
+from django.test import TestCase
+from sispos.parecer.forms import Rds3Form
+
+
+class Rds3FormTest(TestCase):
+    def setUp(self):
+        self.form = Rds3Form()
+
+    def test_form_fields(self):
+        """It must contain the form fields"""
+        fields = ['resultados', 'artigo', 'atividades']
+        for expected in fields:
+            with self.subTest():
+                self.assertIn(expected, list(self.form.fields))
