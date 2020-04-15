3a75ade557ae1c2a46bcb10dd638030567945312
Marcelo Schneider <schneider.fei@gmail.com>
2020-04-15 14:29:09 -0300

    Verifica se exite o campo status - issue #77

--- sispos/parecer/tests/test_parecer_rds3_form.py
+++ sispos/parecer/tests/test_parecer_rds3_form.py
@@ -12,3 +12,7 @@ class Rds3FormTest(TestCase):
         for expected in fields:
             with self.subTest():
                 self.assertIn(expected, list(self.form.fields))
+
+    def test_has_status_attr(self):
+        """It must have status attr"""
+        self.assertIn('status', list(self.form.fields))
