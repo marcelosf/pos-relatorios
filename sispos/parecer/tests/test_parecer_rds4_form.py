13fb58a762dba07c5143abdd3bd03eac3e633f0f
Marcelo Schneider <schneider.fei@gmail.com>
2020-04-15 17:11:21 -0300

    Corrige nome do atributo - issue #74

--- sispos/parecer/tests/test_parecer_rds4_form.py
+++ sispos/parecer/tests/test_parecer_rds4_form.py
@@ -8,7 +8,7 @@ class Rds4FormTest(TestCase):
 
     def test_rds_attr(self):
         """Rds4Form must have attr"""
-        attrs = ['perspectivas', 'status']
+        attrs = ['perspectiva', 'status']
         for expected in attrs:
             with self.subTest():
                 self.assertIn(expected, list(self.form.fields))
