4d99900145d608a2bce400edf85309b842b94c24
Marcelo Schneider <schneider.fei@gmail.com>
2020-04-15 10:44:03 -0300

    Testa status code - issue #71

---
+++ sispos/parecer/tests/test_parecer_views_rds3.py
@@ -0,0 +1,11 @@
+from django.test import TestCase
+from django.shortcuts import resolve_url as r
+
+
+class Rds3ViewsGetTest(TestCase):
+    def setUp(self):
+        self.resp = self.client.get(r('parecer:parecer_rds3_new'))
+
+    def test_status_code(self):
+        """Status code must be 200"""
+        self.assertEqual(200, self.resp.status_code)
