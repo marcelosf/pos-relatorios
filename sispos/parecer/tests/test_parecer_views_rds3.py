94d3a119adcbcf48e8ac77a43a509a30b6f7e043
Marcelo Schneider <schneider.fei@gmail.com>
2020-04-15 11:40:59 -0300

    Verifica se context possui um form - issue #71

--- sispos/parecer/tests/test_parecer_views_rds3.py
+++ sispos/parecer/tests/test_parecer_views_rds3.py
@@ -1,10 +1,18 @@
 from django.test import TestCase
 from django.shortcuts import resolve_url as r
+from sispos.parecer.tests import mock
 
 
 class Rds3ViewsGetTest(TestCase):
     def setUp(self):
-        self.resp = self.client.get(r('parecer:parecer_rds3_new'))
+        mock_user = mock.MockUser()
+        relator = mock_user.make_relator()
+        self.client.force_login(relator)
+        mock_relatorio = mock.MockRelatorio()
+        relatorio_data = mock_relatorio.make_relatorio()
+        relatorio = mock_relatorio.save_relatorio(relatorio_data)
+        self.resp = self.client.get(r('parecer:parecer_rds3_new',
+                                      slug=str(relatorio.uuid)))
 
     def test_status_code(self):
         """Status code must be 200"""
@@ -14,3 +22,7 @@ class Rds3ViewsGetTest(TestCase):
         """It should render parecer_new.py"""
         self.assertTemplateUsed(self.resp, 'parecer_new.html')
 
+    def test_context_form(self):
+        """Context must contain form"""
+        context = self.resp.context
+        self.assertIn('form', context)
