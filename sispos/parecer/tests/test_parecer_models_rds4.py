03727a0a833a921164002fe299343036cecf05e2
Marcelo Schneider <schneider.fei@gmail.com>
2020-04-15 16:26:13 -0300

    Testa persistencia dos dados - issue #73

--- sispos/parecer/tests/test_parecer_models_rds4.py
+++ sispos/parecer/tests/test_parecer_models_rds4.py
@@ -1,5 +1,7 @@
 from django.test import TestCase
 from sispos.parecer.models import Rds4
+from sispos.parecer.tests import mock
+
 
 class Rds4ModelTest(TestCase):
     def setUp(self):
@@ -13,3 +15,14 @@ class Rds4ModelTest(TestCase):
             with self.subTest():
                 msg = '{} not found'.format(expected)
                 self.assertTrue(hasattr(Rds4, expected), msg=msg)
+
+    def test_created(self):
+        mock_user = mock.MockUser()
+        relator = mock_user.make_relator()
+        mock_relatorio = mock.MockRelatorio()
+        relatorio_data = mock_relatorio.make_relatorio()
+        relatorio = mock_relatorio.save_relatorio(relatorio_data)
+        mock_rds4 = mock.MockParecer()
+        data = mock_rds4.make_rds4(relatorio=relatorio, relator=relator)
+        Rds4.objects.create(**data)
+        self.assertTrue(Rds4.objects.exists())
