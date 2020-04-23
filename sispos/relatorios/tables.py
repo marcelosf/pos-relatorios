1b8fc2fb0cbc3e2de4f261fd4473806b96b7bcd0
Marcelo Schneider <schneider.fei@gmail.com>
2020-04-23 17:52:22 -0300

    Refatora definição do botão - issue #83

--- sispos/relatorios/tables.py
+++ sispos/relatorios/tables.py
@@ -2,7 +2,6 @@ import django_tables2 as tables
 from django.shortcuts import resolve_url as r
 from django.utils.html import format_html
 from sispos.relatorios.models import Relatorios
-from sispos.relatorios import models as states
 
 
 class RelatoriosTable(tables.Table):
@@ -10,10 +9,10 @@ class RelatoriosTable(tables.Table):
 
     def render_uuid(self, value):
         if self.user.has_perm('relatorios.change_relatorios'):
-            return self.make_action_button(states.RELATOR_ASSIGNED, value)
+            return self.make_action_button('relator_asigned', value)
 
         if self.user.has_perm('relatorios.add_rds1'):
-            return self.make_action_button(states.PARECER_RELATOR_SUBMITED, value)
+            return self.make_action_button('parecer_relator_submited', value)
 
         self.columns.hide('uuid')
 
@@ -23,7 +22,7 @@ class RelatoriosTable(tables.Table):
     def make_action_button(self, success, slug):
         obj = self.data.data.get(uuid=slug)
         html_class = 'btn-outline-success'
-        button_name = obj.state
+        button_name = obj.get_state_display()
         url = r('relatorios:relatorios_update', slug=slug)
         if not (obj.state == success):
             html_class = 'btn-outline-warning'
